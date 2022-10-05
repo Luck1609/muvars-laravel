5b5e69e48ea10b25c12ad193828d138ccd3eadf7
Nathaniel Obeng <nathanielobeng0@gmail.com>
2022-10-05 20:22:20 +0000

    Next config modified to prevent netlify errors

--- next.config.js
+++ next.config.js
@@ -6,25 +6,25 @@
 
 // module.exports = nextConfig
 
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+// const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-module.exports = (phase, {defaultConfig}) => {
+// module.exports = (phase, {defaultConfig}) => {
 
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      /* development only config options here */
-      reactStrictMode: true,
-      images: {
-        domains: ['localhost', 'localhost:8000'],
-      }
-    }
-  }
+//   if (phase === PHASE_DEVELOPMENT_SERVER) {
+//     return {
+//       /* development only config options here */
+//       reactStrictMode: true,
+//       images: {
+//         domains: ['localhost:3000'],
+//       }
+//     }
+//   }
 
-  return {
-    /* config options for all phases except development here */
-    reactStrictMode: true,
-    images: {
-      domains: [process.env.NEXT_PUBLIC_BACKEND_URL],
-    }
-  }
-}
+//   return {
+//     /* config options for all phases except development here */
+//     reactStrictMode: true,
+//     images: {
+//       domains: [process.env.NEXT_PUBLIC_BACKEND_URL],
+//     }
+//   }
+// }
